ad575553771aff6c2b6d2c9cd62b227fdea9cf01
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-05-07 15:05:46 -0300

    project setup

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/* ------------ SHOW MENU ------------ */
+
+/* --- MENU SHOW --- */
+
+/* --- MENU HIDDEN --- */
+
+/* ------------ REMOVE MENU MOBILE ------------ */
+
+/* ------------ CHANGE BACKGROUND HEADER ------------ */
+
+/* ------------ TESTIMONIAL SWIPER ------------ */
+
+/* ------------ SCROLL SECTIONS ACTIVE LINK ------------ */
+
+/* ------------ PORTFOLIO ITEM FILTER ------------ */
+
+/* ------------  THEME/DISPLAY CUSTOMIZATION ------------ */
+
+/* --- FONTS --- */
+
+/* --- PRIMARY COLORS --- */
+
+/* --- THEME BACKGROUND --- */
+
+/* ------------  ------------ */
