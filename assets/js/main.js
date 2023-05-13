3ce052f56d43e183e457b91f02ce00d33729c819
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-05-13 12:06:27 -0300

    navbar in javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,51 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js';
 
-/* ------------ SHOW MENU ------------ */
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
-/* --- MENU SHOW --- */
+/* ------------ SHOW MENU ------------ */
+// validate if constant exists
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
 
 /* --- MENU HIDDEN --- */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /* ------------ REMOVE MENU MOBILE ------------ */
+const navLinks = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  // when we click on each nav link, we remove the show menu class
+  navMenu.classList.remove('show-menu');
+}
+navLinks.forEach((link) => link.addEventListener('click', linkAction));
 
 /* ------------ CHANGE BACKGROUND HEADER ------------ */
+function scrollHeader() {
+  const header = document.getElementById('header');
+  // when the scroll is greater then 80 viewport height, add the class scroll header to the tag header
+  if (this.scrollY >= 80) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
+}
+window.addEventListener('scroll', scrollHeader);
 
 /* ------------ TESTIMONIAL SWIPER ------------ */
 const swiper = new Swiper('.testimonial-wrapper', {
+  spaceBetween: 30,
   loop: 'true',
+
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
