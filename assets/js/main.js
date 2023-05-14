18dc1cc4fbbaa2366f977fee76b1fea2e0e08f0b
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-05-14 18:34:45 -0300

    theme customization in javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,13 @@ const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
   navClose = document.getElementById('nav-close');
 
+// remove class from array of spans selectors
+const removeSelectors = (array, classRemove) => {
+  array.forEach((element) => {
+    element.classList.remove(classRemove);
+  });
+};
+
 /* ------------ SHOW MENU ------------ */
 // validate if constant exists
 if (navToggle) {
@@ -22,8 +29,12 @@ if (navClose) {
 /* ------------ REMOVE MENU MOBILE ------------ */
 const navLinks = document.querySelectorAll('.nav-link');
 
-function linkAction() {
+function linkAction(event) {
+  removeSelectors(navLinks, 'active-link');
+
   const navMenu = document.getElementById('nav-menu');
+  event.target.classList.toggle('active-link');
+
   // when we click on each nav link, we remove the show menu class
   navMenu.classList.remove('show-menu');
 }
@@ -57,11 +68,107 @@ const swiper = new Swiper('.testimonial-wrapper', {
 /* ------------ PORTFOLIO ITEM FILTER ------------ */
 
 /* ------------  THEME/DISPLAY CUSTOMIZATION ------------ */
+const theme = document.querySelector('#theme-change');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+const colorPallete = document.querySelectorAll('.choose-color span');
+
+var root = document.querySelector(':root');
+
+const bg1 = document.querySelector('.bg-1');
+const bg2 = document.querySelector('.bg-2');
+const bg3 = document.querySelector('.bg-3');
+
+/* --- MODAL --- */
+// open modal
+const openThemeModal = () => {
+  themeModal.style.display = 'grid';
+};
+theme.addEventListener('click', openThemeModal);
+
+// close modal
+const closeThemeModal = (event) => {
+  if (event.target.classList.contains('customize-theme')) {
+    themeModal.style.display = 'none';
+  }
+};
+themeModal.addEventListener('click', closeThemeModal);
 
 /* --- FONTS --- */
+fontSizes.forEach((size) => {
+  size.addEventListener('click', () => {
+    removeSelectors(fontSizes, 'active');
+
+    let fontSize;
+
+    size.classList.toggle('active');
+
+    if (size.classList.contains('font-size-1')) {
+      fontSize = '12px';
+    } else if (size.classList.contains('font-size-2')) {
+      fontSize = '14px';
+    } else if (size.classList.contains('font-size-3')) {
+      fontSize = '16px';
+    } else if (size.classList.contains('font-size-4')) {
+      fontSize = '18px';
+    }
+
+    // change font size of the root html element
+    document.querySelector('html').style.fontSize = fontSize;
+  });
+});
 
 /* --- PRIMARY COLORS --- */
+colorPallete.forEach((color) => {
+  color.addEventListener('click', () => {
+    removeSelectors(colorPallete, 'active');
+
+    let primaryHue;
+
+    color.classList.toggle('active');
+
+    if (color.classList.contains('color-1')) {
+      primaryHue = 252;
+    } else if (color.classList.contains('color-2')) {
+      primaryHue = 52;
+    } else if (color.classList.contains('color-3')) {
+      primaryHue = 352;
+    } else if (color.classList.contains('color-4')) {
+      primaryHue = 152;
+    } else if (color.classList.contains('color-5')) {
+      primaryHue = 202;
+    }
+
+    root.style.setProperty('--primary-color-hue', primaryHue);
+  });
+});
 
 /* --- THEME BACKGROUND --- */
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+const changeBG = (element, color1, color2, color3, array) => {
+  element.addEventListener('click', () => {
+    darkColorLightness = color1;
+    whiteColorLightness = color2;
+    lightColorLightness = color3;
+
+    // remove active class from the others
+    removeSelectors(array, 'active');
+
+    // add active class
+    element.classList.add('active');
+
+    // change background color
+    root.style.setProperty('--dark-color-lightness', darkColorLightness);
+    root.style.setProperty('--light-color-lightness', lightColorLightness);
+    root.style.setProperty('--white-color-lightness', whiteColorLightness);
+  });
+};
+
+changeBG(bg1, '17%', '100%', '92%', [bg2, bg3]);
+changeBG(bg2, '95%', '20%', '17%', [bg1, bg3]);
+changeBG(bg3, '95%', '10%', '0%', [bg1, bg2]);
 
 /* ------------  ------------ */
