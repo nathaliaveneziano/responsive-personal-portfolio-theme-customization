f158ed6718ba2728aea85f98095d0955b654b791
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-05-15 20:23:21 -0300

    scroll bar in javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,6 +64,37 @@ const swiper = new Swiper('.testimonial-wrapper', {
 });
 
 /* ------------ SCROLL SECTIONS ACTIVE LINK ------------ */
+// get all sections that have an id defined
+const sections = document.querySelectorAll('section[id]');
+
+// add an event listener listening for scroll
+window.addEventListener('scroll', navHighlighter);
+
+function navHighlighter() {
+  // get current scroll position
+  let scrollY = window.pageYOffset;
+
+  // now we loop through sections to get height, top and ID values for each
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id');
+
+    /**
+     * - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+     * - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as a selector
+     */
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav-menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav-menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
 
 /* ------------ PORTFOLIO ITEM FILTER ------------ */
 const filterContainer = document.querySelector('.portfolio-filter-inner'),
